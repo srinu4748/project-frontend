dbf4f623cbd4d8c8edd019b091cbc58e69bb30ef
srinu4748 <149197453+srinu4748@users.noreply.github.com>
2024-04-28 12:16:57 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Welcome to Event Planning Website,we're passionate about turning your celebrations into unforgettable moments. With our MERN-powered platform, we offer a seamless and personalized event planning experience. 
+      From birthdays to weddings, and everything in between, our team is dedicated to bringing your vision to life.At Event Planning Website, we understand the importance of every detail. 
+      Our mission is to make event planning effortless, so you can focus on creating memories that last a lifetime. With innovative technology and a commitment to excellence, we're here to make your event dreams a reality.
+      Join us on this journey of celebration and discovery. Let's create something extraordinary together.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="banner">
+        <div className="title">
+          <h1>SRINU's</h1>
+          <p>Events and Weddings</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
